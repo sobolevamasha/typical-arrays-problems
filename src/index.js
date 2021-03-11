91e4184352ffd3fbf56565ca6553b606913f7a38
Masha <77979222+sobolevamasha@users.noreply.github.com>
2021-03-11 14:03:25 +0300

    feat: implement 3 functions: max, min, and avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-
 exports.min = function min (array) {
-  return 0;
+  if (array == undefined || array.length == 0) {return 0};
+  let min = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (min > array[i]) {min = array[i];}
+  }
+
+  return min;
 }
+
 
 exports.max = function max (array) {
-  return 0;
+  if (array == undefined || array.length == 0) {return 0};
+
+  let max = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (max < array[i]) {max = array[i];}
+  }
+
+  return max;
 }
 
+
 exports.avg = function avg (array) {
-  return 0;
+  console.log(array);
+  if (array == undefined || array.length == 0) {return 0};
+  
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return (sum/array.length);
 }
